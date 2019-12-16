bd67491c865992be521a6eaeff963ba97f4f0a8c
GambuzX <mg_mesquita@hotmail.com>
2019-12-16 13:22:12 +0000

    variables and domains defined

---
+++ battleships.pl
@@ -0,0 +1,41 @@
+:-use_module(library(clpfd)).
+
+battleships :-
+    Shapes = [S1, S2, S3, S4, S5, S6, S7, S8, S9, S10],
+    Positions = [
+        X1, X2, X3, X4, X5, X6, X7, X8, X9, X10,
+        Y1, Y2, Y3, Y4, Y5, Y6, Y7, Y8, Y9, Y10
+    ],
+
+    % 1 indexed!!!!
+    domain(Positions, 1, 10),
+
+    domain([S1, S2, S3, S4], 1, 1),
+    domain([S5, S6, S7], 2, 3),
+    domain([S8, S9], 4, 5),
+    domain([S10], 6, 7),
+
+    Ships = [
+        object(1, S1, [X1, Y1]),
+        object(2, S2, [X2, Y2]),
+        object(3, S3, [X3, Y3]),
+        object(4, S4, [X4, Y4]),
+        object(5, S5, [X5, Y5]),
+        object(6, S6, [X6, Y6]),
+        object(7, S7, [X7, Y7]),
+        object(8, S8, [X8, Y8]),
+        object(9, S9, [X9, Y9]),
+        object(10, S10, [X10, Y10])
+    ],
+
+    Shapes = [
+        sbox(1, [0,0], [1,1]),
+        sbox(2, [0,0], [1, 2]),
+        sbox(3, [0,0], [2, 1]),
+        sbox(4, [0,0], [3, 1]),
+        sbox(5, [0,0], [1, 3]),
+        sbox(6, [0,0], [4, 1]),
+        sbox(7, [0,0], [1, 4])
+    ].
+
+
