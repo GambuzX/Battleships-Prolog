0d3ddaa178d272ea24fb55d157161009bd2340b2
GambuzX <mg_mesquita@hotmail.com>
2019-12-29 23:25:26 +0000

    Turning solver more generic

--- battleships.pl
+++ battleships.pl
@@ -107,6 +107,12 @@ choose_board(6).
 choose_board(7) :-
     battleships_menu, !.
 
+test :-
+    HorizontalCounts = [4, 1, 3, 0, 3, 0, 4, 0, 2, 3],
+    VerticalCounts = [1, 4, 1, 0, 4, 4, 1, 3, 1, 1],
+    solve_battleships(10/10, 10, [], HorizontalCounts, VerticalCounts).
+
+
 
 /*
     TODO
@@ -128,18 +134,14 @@ choose_board(7) :-
     of the ships on the rows are inverted.
     The board should be imagined with increasing y and figures growing upwards.
 */
-solve_battleships_10x10(ShipsShapes, Positions) :-
+solve_battleships(Rows/Columns, NShips, Board, HorizontalCounts, VerticalCounts) :-
     ShipsShapes = [S1, S2, S3, S4, S5, S6, S7, S8, S9, S10],
-    Positions = [
-        X1, X2, X3, X4, X5, X6, X7, X8, X9, X10,
-        Y1, Y2, Y3, Y4, Y5, Y6, Y7, Y8, Y9, Y10
-    ],
-
-    HorizontalCounts = [4, 1, 3, 0, 3, 0, 4, 0, 2, 3],
-    VerticalCounts = [1, 4, 1, 0, 4, 4, 1, 3, 1, 1],
+    X_Coords = [X1, X2, X3, X4, X5, X6, X7, X8, X9, X10],
+    Y_Coords = [Y1, Y2, Y3, Y4, Y5, Y6, Y7, Y8, Y9, Y10],
 
     % 1 indexed!!!!
-    domain(Positions, 1, 10),
+    domain(X_Coords, 1, Columns),
+    domain(Y_Coords, 1, Rows),
 
     domain([S1, S2, S3, S4], 1, 1),
     domain([S5, S6, S7], 2, 3),
@@ -158,6 +160,7 @@ solve_battleships_10x10(ShipsShapes, Positions) :-
         object(9, S9, [X9, Y9]),
         object(10, S10, [X10, Y10])
     ],
+    getShipsIDs(Ships, ShipsIDs),
 
     % horizontal and vertical shapes for each ship size
     Shapes = [
@@ -215,109 +218,7 @@ solve_battleships_10x10(ShipsShapes, Positions) :-
                     #\ tooclose(Obj1, Obj2, Shape1, Shape2, 2)))), % check vertically
 
         % for all combinations of different objects
-        (forall(Obj1, objects([1,2,3,4,5,6,7,8,9,10]), % TODO these ids must come from outside and not be hardcoded
-            forall(Obj2, objects([1,2,3,4,5,6,7,8,9,10]),
-                % if different objects, must be apart 1 unit
-                (Obj2^oid #>= Obj1^oid) #\/ apart(Obj1, Obj2))))
-    ],
-    
-    geost(Ships, Shapes, Options, Rules),
-    %force_horizontal_ships_counts(1, HorizontalCounts, Ships, Shapes),
-    append(Positions, ShipsShapes, AllVars),
-    labeling([ffc, median], AllVars),
-    write(ShipsShapes),
-    write(Positions).
-
-solve_battleships_7x7(ShipsShapes, Positions) :-
-    ShipsShapes = [S1, S2, S3, S4, S5, S6],
-    Positions = [
-        X1, X2, X3, X4, X5, X6,
-        Y1, Y2, Y3, Y4, Y5, Y6
-    ],
-
-    HorizontalCounts = [1, 0, 2, 1, 1, 3, 2],
-    VerticalCounts = [3, 0, 2, 1, 2, 0, 2],
-
-    % 1 indexed!!!!
-    domain(Positions, 1, 7),
-
-    domain([S1, S2, S3], 1, 1),
-    domain([S4, S5], 2, 3),
-    domain([S6], 4, 5),
-
-    X2 #>= X1,
-    Y2 #>= Y1,
-    X3 #>= X2,
-    Y3 #>= Y2,
-    
-    X5 #>= X4,
-    Y5 #>= Y4,
-
-    Ships = [
-        object(1, S1, [X1, Y1]),
-        object(2, S2, [X2, Y2]),
-        object(3, S3, [X3, Y3]),
-        object(4, S4, [X4, Y4]),
-        object(5, S5, [X5, Y5]),
-        object(6, S6, [X6, Y6])
-    ],
-    ShipsIDs = [1,2,3,4,5,6],
-
-    % horizontal and vertical shapes for each ship size
-    Shapes = [
-        sbox(1, [0,0], [1, 1]),
-        sbox(2, [0,0], [1, 2]),
-        sbox(3, [0,0], [2, 1]),
-        sbox(4, [0,0], [1, 3]),
-        sbox(5, [0,0], [3, 1])
-    ],
-
-    Options = [
-        /*
-            limit all coordinates of the objects to be in range [1,10]
-            geost only guarantees that the object origin is inside the specified domain
-        */
-        bounding_box([1, 1], [8, 8])
-    ],
-
-    /*
-        Geost Rules (https://web.imt-atlantique.fr/x-info/ppc/bib/pub/carlsson-al-CP-2008.pdf)
-
-        k is the dimension, in this case 2.
-        Below, d means the dimension to extract value.
-
-        Shapes' properties (integers):
-            - s.sid : shape id
-            - s.t[d] : shift offset (1 <= d <= k)
-            - s.l[d] : sizes (s.l[d] > 0 and 1 <= d <= k)
-
-        Objects' properties:
-            - o.oid : unique object id (integer)
-            - o.sid : shape id (integer if fixed shape, or domain variable for polymorphic objects)
-            - o.x[d] : origin (1 <= d <= k)
-        
-        card(quantified variable, list of terms, lower bound, upper bound, fol)
-    */
-    Rules = [ 
-        /* sum of origin value with shape offset in dimension D*/
-        (origin(Obj, Shape, Dim) ---> Obj^x(Dim) + Shape^t(Dim)),
-
-        /* sum of origin value with shape offset and size in dimension D */
-        (end(Obj, Shape, Dim) ---> Obj^x(Dim) + Shape^t(Dim) + Shape^l(Dim)),
-
-        (tooclose(Obj1, Obj2, Shape1, Shape2, Dim) --->
-            end(Obj1, Shape1, Dim) + 1 #> origin(Obj2, Shape2, Dim) #/\
-            end(Obj2, Shape2, Dim) + 1 #> origin(Obj1, Shape1, Dim)),
-
-        % assure objects O1 and O2 are apart at least 1 unit
-        (apart(Obj1, Obj2) --->
-            forall(Shape1, sboxes([Obj1^sid]), % shape boxes of object O1, could be more than 1
-                forall(Shape2, sboxes([Obj2^sid]),% shape boxes of object O2
-                    #\ tooclose(Obj1, Obj2, Shape1, Shape2, 1) #\/ % check horizontally
-                    #\ tooclose(Obj1, Obj2, Shape1, Shape2, 2)))), % check vertically
-
-        % for all combinations of different objects
-        (forall(Obj1, objects(ShipsIDs),
+        (forall(Obj1, objects(ShipsIDs), % TODO these ids must come from outside and not be hardcoded
             forall(Obj2, objects(ShipsIDs),
                 % if different objects, must be apart 1 unit
                 (Obj2^oid #>= Obj1^oid) #\/ apart(Obj1, Obj2))))
@@ -326,10 +227,19 @@ solve_battleships_7x7(ShipsShapes, Positions) :-
     geost(Ships, Shapes, Options, Rules),
     force_horizontal_ships_counts(1, HorizontalCounts, Ships),
     force_vertical_ships_counts(1, VerticalCounts, Ships),
-    append(Positions, ShipsShapes, AllVars),
-    labeling([], AllVars),
+
+    append([ShipsShapes, X_Coords, Y_Coords], AllVars),
+    labeling([ffc, median], AllVars),
     write(ShipsShapes),
-    write(Positions).
+    write(X_Coords),
+    write(Y_Coords).
+
+
+/* Collect the IDs of the objects in a list */
+getShipsIDs([], []).
+getShipsIDs([object(ID, _, _) | Rest], [ID | RestIDs]) :-
+    getShipsIDs(Rest, RestIDs).
+
 
 /*
     Count number of ships in a row
@@ -341,7 +251,9 @@ count_ships_in_row(Row, [object(_, CurrShapeID, [_, Y]) | RestShips], Count) :-
     (CurrShapeID #= 2 #/\ Width #= 1 #/\ Height #= 2) #\/
     (CurrShapeID #= 3 #/\ Width #= 2 #/\ Height #= 1) #\/
     (CurrShapeID #= 4 #/\ Width #= 1 #/\ Height #= 3) #\/
-    (CurrShapeID #= 5 #/\ Width #= 3 #/\ Height #= 1),
+    (CurrShapeID #= 5 #/\ Width #= 3 #/\ Height #= 1) #\/
+    (CurrShapeID #= 6 #/\ Width #= 1 #/\ Height #= 4) #\/
+    (CurrShapeID #= 7 #/\ Width #= 4 #/\ Height #= 1),
 
     EndY #= Y + Height - 1,
 
@@ -379,7 +291,9 @@ count_ships_in_col(Col, [object(_, CurrShapeID, [X, _]) | RestShips], Count) :-
     (CurrShapeID #= 2 #/\ Width #= 1 #/\ Height #= 2) #\/
     (CurrShapeID #= 3 #/\ Width #= 2 #/\ Height #= 1) #\/
     (CurrShapeID #= 4 #/\ Width #= 1 #/\ Height #= 3) #\/
-    (CurrShapeID #= 5 #/\ Width #= 3 #/\ Height #= 1),
+    (CurrShapeID #= 5 #/\ Width #= 3 #/\ Height #= 1) #\/
+    (CurrShapeID #= 6 #/\ Width #= 1 #/\ Height #= 4) #\/
+    (CurrShapeID #= 7 #/\ Width #= 4 #/\ Height #= 1),
 
     EndX #= X + Width - 1,
 
