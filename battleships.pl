c7b0355875e84800a4c29f3bc71d8c595e3642c8
GambuzX <mg_mesquita@hotmail.com>
2019-12-29 17:49:08 +0000

    Forcing horizontal counts is working

--- battleships.pl
+++ battleships.pl
@@ -127,7 +127,7 @@ choose_board(7) :-
     the board so that the y increases upwards. As such, the values for the sum
     of the ships on the rows are inverted.
 */
-solve_battleships(ShipsShapes, Positions) :-
+solve_battleships_10x10(ShipsShapes, Positions) :-
     ShipsShapes = [S1, S2, S3, S4, S5, S6, S7, S8, S9, S10],
     Positions = [
         X1, X2, X3, X4, X5, X6, X7, X8, X9, X10,
@@ -227,17 +227,98 @@ solve_battleships(ShipsShapes, Positions) :-
     write(ShipsShapes),
     write(Positions).
 
-/*
-    Find shape from list Shapes with ID equal to ShapeID
-*/
-find_shape(ShapeID, [CurrShape | Rest], CurrShape) :-
-    sbox(CurrShapeID, _, _) = CurrShape,
-    ShapeID #= CurrShapeID.
+solve_battleships_7x7(ShipsShapes, Positions) :-
+    ShipsShapes = [S1, S2, S3, S4, S5, S6],
+    Positions = [
+        X1, X2, X3, X4, X5, X6,
+        Y1, Y2, Y3, Y4, Y5, Y6
+    ],
 
-find_shape(ShapeID, [CurrShape | Rest], Res) :-
-    sbox(ShapeID2, _, _) = CurrShape,
-    ShapeID #\= ShapeID2,
-    find_shape(ShapeID, Rest, Res).
+    HorizontalCounts = [1, 0, 2, 1, 1, 3, 2],
+    VerticalCounts = [3, 0, 2, 1, 2, 0, 2],
+
+    % 1 indexed!!!!
+    domain(Positions, 1, 7),
+
+    domain([S1, S2, S3], 1, 1),
+    domain([S4, S5], 2, 3),
+    domain([S6], 4, 5),
+
+    Ships = [
+        object(1, S1, [X1, Y1]),
+        object(2, S2, [X2, Y2]),
+        object(3, S3, [X3, Y3]),
+        object(4, S4, [X4, Y4]),
+        object(5, S5, [X5, Y5]),
+        object(6, S6, [X6, Y6])
+    ],
+
+    % horizontal and vertical shapes for each ship size
+    Shapes = [
+        sbox(1, [0,0], [1, 1]),
+        sbox(2, [0,0], [1, 2]),
+        sbox(3, [0,0], [2, 1]),
+        sbox(4, [0,0], [1, 3]),
+        sbox(5, [0,0], [3, 1])
+    ],
+
+    Options = [
+        /*
+            limit all coordinates of the objects to be in range [1,10]
+            geost only guarantees that the object origin is inside the specified domain
+        */
+        bounding_box([1, 1], [8, 8])
+    ],
+
+    /*
+        Geost Rules (https://web.imt-atlantique.fr/x-info/ppc/bib/pub/carlsson-al-CP-2008.pdf)
+
+        k is the dimension, in this case 2.
+        Below, d means the dimension to extract value.
+
+        Shapes' properties (integers):
+            - s.sid : shape id
+            - s.t[d] : shift offset (1 <= d <= k)
+            - s.l[d] : sizes (s.l[d] > 0 and 1 <= d <= k)
+
+        Objects' properties:
+            - o.oid : unique object id (integer)
+            - o.sid : shape id (integer if fixed shape, or domain variable for polymorphic objects)
+            - o.x[d] : origin (1 <= d <= k)
+        
+        card(quantified variable, list of terms, lower bound, upper bound, fol)
+    */
+    Rules = [ 
+        /* sum of origin value with shape offset in dimension D*/
+        (origin(Obj, Shape, Dim) ---> Obj^x(Dim) + Shape^t(Dim)),
+
+        /* sum of origin value with shape offset and size in dimension D */
+        (end(Obj, Shape, Dim) ---> Obj^x(Dim) + Shape^t(Dim) + Shape^l(Dim)),
+
+        (tooclose(Obj1, Obj2, Shape1, Shape2, Dim) --->
+            end(Obj1, Shape1, Dim) + 1 #> origin(Obj2, Shape2, Dim) #/\
+            end(Obj2, Shape2, Dim) + 1 #> origin(Obj1, Shape1, Dim)),
+
+        % assure objects O1 and O2 are apart at least 1 unit
+        (apart(Obj1, Obj2) --->
+            forall(Shape1, sboxes([Obj1^sid]), % shape boxes of object O1, could be more than 1
+                forall(Shape2, sboxes([Obj2^sid]),% shape boxes of object O2
+                    #\ tooclose(Obj1, Obj2, Shape1, Shape2, 1) #\/ % check horizontally
+                    #\ tooclose(Obj1, Obj2, Shape1, Shape2, 2)))), % check vertically
+
+        % for all combinations of different objects
+        (forall(Obj1, objects([1,2,3,4,5,6]), % TODO these ids must come from outside and not be hardcoded
+            forall(Obj2, objects([1,2,3,4,5,6]),
+                % if different objects, must be apart 1 unit
+                (Obj2^oid #>= Obj1^oid) #\/ apart(Obj1, Obj2))))
+    ],
+    
+    geost(Ships, Shapes, Options, Rules),
+    force_horizontal_ships_counts(1, HorizontalCounts, Ships, Shapes),
+    append(Positions, ShipsShapes, AllVars),
+    labeling([], AllVars),
+    write(ShipsShapes),
+    write(Positions).
 
 /*
     Count number of ships in a row
@@ -245,10 +326,13 @@ find_shape(ShapeID, [CurrShape | Rest], Res) :-
 count_ships_in_row(_, [], _, 0).
 count_ships_in_row(Row, [object(_, CurrShapeID, [_, Y]) | RestShips], Shapes, Count) :-    
     
-    find_shape(CurrShapeID, Shapes, CurrShape),
-    sbox(_, _, [_, Height]) = CurrShape,
+    (CurrShapeID #= 1 #/\ Height #= 1) #\/
+    (CurrShapeID #= 2 #/\ Height #= 2) #\/
+    (CurrShapeID #= 3 #/\ Height #= 1) #\/
+    (CurrShapeID #= 4 #/\ Height #= 3) #\/
+    (CurrShapeID #= 5 #/\ Height #= 1),
 
-    EndY #= Y + Height,
+    EndY #= Y + Height - 1,
 
     (Y #=< Row #/\ EndY #>= Row) #<=> Matched,
     Count #= NextCount + Matched,
