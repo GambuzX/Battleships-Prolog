ef73827e316c64e008fb35fd7a9ab38e9b6a1306
Pedro Esteves <up201705160@fe.up.pt>
2020-04-03 23:13:46 +0100

    Changed tests

--- test.pl
+++ test.pl
@@ -11,21 +11,15 @@ test_solve_battleships(FileListName, VarLabelFile, ValLabelFile, OrdLabelFile) :
 test_solve_battleships_files([], _, _, _) :- !.
 
 test_solve_battleships_files([File|FileList], VarList, ValList, OrdList) :- 
-    test_solver(File, 3, VarList, ValList, OrdList),
-    test_solve_battleships_files(FileList, VarList, ValList, OrdList).
+    test_solver(File, VarList, ValList, OrdList),
+    test_solve_battleships_files(FileList, VarList, ValList, OrdList), !.
 
-
-test_solver(_, 0, _, _, _) :- !.
-
-test_solver(FileName, N, VarList, ValList, OrdList) :-
+test_solver(FileName, VarList, ValList, OrdList) :-
     get_battleships_board(FileName, NShips, Row/Column, Board, RowVal, ColVal),
     get_other_solver_values(Board, WaterBlocks, SubmarineBlocks, MiddleBlocks, LeftBlocks, BottomBlocks, RightBlocks, TopBlocks),
     run_tests(FileName, Row/Column, NShips, RowVal, ColVal, 
               WaterBlocks, SubmarineBlocks, MiddleBlocks, LeftBlocks, BottomBlocks, RightBlocks, TopBlocks,
-              VarList, ValList, OrdList),
-    
-    N1 is N - 1,
-    test_solver(FileName, N1, VarList, ValList, OrdList).
+              VarList, ValList, OrdList), !.
 
 
 get_other_solver_values(Board, WaterBlocks, SubmarineBlocks, MiddleBlocks, LeftBlocks, BottomBlocks, RightBlocks, TopBlocks) :-
@@ -36,7 +30,7 @@ get_other_solver_values(Board, WaterBlocks, SubmarineBlocks, MiddleBlocks, LeftB
     get_blocks(Board, l, NumRows, LeftBlocks),
     get_blocks(Board, b, NumRows, BottomBlocks),
     get_blocks(Board, r, NumRows, RightBlocks),
-    get_blocks(Board, t, NumRows, TopBlocks).
+    get_blocks(Board, t, NumRows, TopBlocks), !.
 
 run_tests(_, _, _, _, _, _, _, _, _, _, _, _, [], _, _) :- !.
 
@@ -48,7 +42,7 @@ run_tests(FileName, Rows/Columns, NShips, HorizontalCounts, VerticalCounts,
           VarLabel, ValLabelL, OrdLabelL),
     run_tests(FileName, Rows/Columns, NShips, HorizontalCounts, VerticalCounts, 
           WaterBlocks, SubmarineBlocks, MiddleBlocks, LeftBlocks, BottomBlocks, RightBlocks, TopBlocks, 
-          VarLabelL, ValLabelL, OrdLabelL).
+          VarLabelL, ValLabelL, OrdLabelL), !.
 
 run_variable_tests(_, _, _, _, _, _, _, _, _, _, _, _, _, [], _) :- !.
 
@@ -60,16 +54,26 @@ run_variable_tests(FileName, Rows/Columns, NShips, HorizontalCounts, VerticalCou
           VarLabel, ValLabel, OrdLabelL),
     run_variable_tests(FileName, Rows/Columns, NShips, HorizontalCounts, VerticalCounts, 
           WaterBlocks, SubmarineBlocks, MiddleBlocks, LeftBlocks, BottomBlocks, RightBlocks, TopBlocks, 
-          VarLabel, ValLabelL, OrdLabelL).
+          VarLabel, ValLabelL, OrdLabelL), !.
 
 run_value_tests(_, _, _, _, _, _, _, _, _, _, _, _, _, _, []) :- !.
 
 run_value_tests(FileName, Rows/Columns, NShips, HorizontalCounts, VerticalCounts, 
           WaterBlocks, SubmarineBlocks, MiddleBlocks, LeftBlocks, BottomBlocks, RightBlocks, TopBlocks, 
           VarLabel, ValLabel, [OrdLabel | OrdLabelL]) :-
+      run_order_tests(FileName, Rows/Columns, NShips, WaterBlocks, SubmarineBlocks, MiddleBlocks, LeftBlocks, BottomBlocks, RightBlocks, TopBlocks, 
+                      HorizontalCounts, VerticalCounts, VarLabel, ValLabel, OrdLabel, 3),
+      run_value_tests(FileName, Rows/Columns, NShips, HorizontalCounts, VerticalCounts, 
+                      WaterBlocks, SubmarineBlocks, MiddleBlocks, LeftBlocks, BottomBlocks, RightBlocks, TopBlocks, 
+                      VarLabel, ValLabel, OrdLabelL), !.    
+
+run_order_tests(_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 0) :- !.
+
+run_order_tests(FileName, Rows/Columns, NShips, WaterBlocks, SubmarineBlocks, MiddleBlocks, LeftBlocks, BottomBlocks, RightBlocks, TopBlocks, 
+                      HorizontalCounts, VerticalCounts, VarLabel, ValLabel, OrdLabel, N) :-
     solve_battleships(Rows/Columns, NShips, WaterBlocks, SubmarineBlocks, MiddleBlocks, LeftBlocks, BottomBlocks, RightBlocks, TopBlocks, 
                       HorizontalCounts, VerticalCounts, VarLabel, ValLabel, OrdLabel, Time),
     save_time(FileName, VarLabel, ValLabel, OrdLabel, Time),
-    run_value_tests(FileName, Rows/Columns, NShips, HorizontalCounts, VerticalCounts, 
-          WaterBlocks, SubmarineBlocks, MiddleBlocks, LeftBlocks, BottomBlocks, RightBlocks, TopBlocks, 
-          VarLabel, ValLabel, OrdLabelL).    
+    N1 is N - 1,
+    run_order_tests(Rows/Columns, NShips, WaterBlocks, SubmarineBlocks, MiddleBlocks, LeftBlocks, BottomBlocks, RightBlocks, TopBlocks, 
+                    HorizontalCounts, VerticalCounts, VarLabel, ValLabel, OrdLabel, N1), !.
